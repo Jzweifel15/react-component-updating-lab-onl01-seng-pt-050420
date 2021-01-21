d8639f11c21c73988f7b99f3d76d524ddd28ba43
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-21 19:49:35 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -22,6 +22,11 @@ class Timer extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+  
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + 
+        Math.floor(Math.random() * 16777215).toString(16);
+  }
 
   render() {
     const { time, color, logText } = this.state;
